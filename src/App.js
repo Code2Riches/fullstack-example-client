9d167829fc62641afd71ca2cde773e3d4af96ee0
Code2Riches <chris.benites@gmail.com>
2022-11-01 10:27:26 -0400

    Init Fullstack Example commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import './App.css';
+
+const urlEndpoint = "http://localhost:4000"
+
+function App() {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(()=>{
+    const fetchBlogs = async () => {
+      const result = await fetch(`${urlEndpoint}/blogs/all`)
+      const fetchedBlogs = await result.json()
+      // console.log(fetchedBlogs)
+      setBlogs(fetchedBlogs.blogs)
+    }
+    fetchBlogs()
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {blogs.map((blog, index)=>{
+          return (
+            <div key={index}>
+              {blog.title}
+            </div>
+          )
+        })}
+      </header>
+    </div>
+  );
+}
+
+export default App;
